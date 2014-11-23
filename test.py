fcf74867d3f60bee77f71900cfda484831c3c3cb
Bill Boyer <william.boyer.nyc@gmail.com>
2014-11-23 13:28:22 -0500

    Initial commit.

---
+++ test.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+color('green', 'blue')
+speed(11)
+
+step = 20
+angle = 10
+quadrant = 1
+
+begin_fill()
+while True:
+    forward(step)
+    left(angle)
+    if abs(pos()) < 1:
+        quadrant = quadrant + 1
+        if quadrant > 4:
+            break
+        else:
+            left(90)
+end_fill()
+done()
